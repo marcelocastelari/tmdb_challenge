888bb0303536f9c8d416febec3cb3dc8bdc100ae
marcelocastelari <marcelocastelari@gmail.com>
2024-05-19 20:23:05 -0300

    feat: add auth middleware to validate jwt on private requests

--- backend/src/App.ts
+++ backend/src/App.ts
@@ -1,28 +1,18 @@
 import express from 'express';
 import cors from 'cors';
-import { AuthController } from './controllers/AuthController';
+import authRoutes from './routes/AuthRoutes';
+import userMovieListRoutes from './routes/UserMovieListRoutes';
 
 const app = express();
 const port = 8000;
 
-const authController = new AuthController();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-app.post('/login', async (req, res) => {
-    const { username, password } = req.body;
-    const result = await authController.login(username, password);
-    res.status(result.status).json({ message: result.message });
-});
-
-app.post('/register', async (req, res) => {
-    const { username, password } = req.body;
-    console.log(req.body);
-    const result = await authController.register(username, password);
-    res.status(result.status).json({ message: result.message });
-});
+app.use('/auth', authRoutes);
+app.use('/userMovieList', userMovieListRoutes);
 
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
